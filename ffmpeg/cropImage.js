c4cadfa5d7fc47baf6a8ea6746f9fc3a73578cc9
Pi <piisthebestthingintheuniverse@gmail.com>
2023-11-11 14:52:02 -0800

    change image size

--- ffmpeg/cropImage.js
+++ ffmpeg/cropImage.js
@@ -11,7 +11,9 @@ export function cropToSquare(/** @type {FFmpeg} */ ffmpeg, /** @type {String} */
 
 		const ext = type.replace("image/", "");
 		await ffmpeg.writeFile(`avatarpreview.${ext}`, data);
-		await ffmpeg.exec(["-i", `avatarpreview.${ext}`, "-vf", "scale=720:720:force_original_aspect_ratio=increase,crop=720:720", `avatarpreviewcropped.${ext}`]);
+
+		// run ffmpeg -i avatarpreview.png -vf "scale=512:512:force_original_aspect_ratio=increase,crop=512:512" avatarpreviewcropped.png
+		await ffmpeg.exec(["-i", `avatarpreview.${ext}`, "-vf", "scale=512:512:force_original_aspect_ratio=increase,crop=512:512", `avatarpreviewcropped.${ext}`]);
 
 		const res = await ffmpeg.readFile(`avatarpreviewcropped.${ext}`);
 		const reader = new FileReader();
@@ -19,7 +21,5 @@ export function cropToSquare(/** @type {FFmpeg} */ ffmpeg, /** @type {String} */
 		reader.onload = () => {
 			resolve(reader.result);
 		};
-
-		// ffmpeg.terminate();
 	});
 }
