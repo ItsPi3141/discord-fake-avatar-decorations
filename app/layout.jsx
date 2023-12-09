a8855d40b29d40cd0c670451c987e6141be79f37
Pi <piisthebestthingintheuniverse@gmail.com>
2023-12-09 10:50:42 -0800

    change metadata to opengraph meta tags

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,15 +2,38 @@
 
 import "./globals.css";
 
-export const metadata = {
-	title: "Discord Fake Avatar Decorations",
-	description: "Get Discord avatar decorations without spending money"
-};
-
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
-			<head></head>
+			<head>
+				<title>Discord Fake Avatar Decorations</title>
+				<meta
+					name="description"
+					content="Get Discord avatar decorations without spending money"
+				/>
+
+				<meta
+					property="og:type"
+					content="website"
+				/>
+				<meta
+					property="og:title"
+					content="Discord Fake Avatar Decorations"
+				/>
+				<meta
+					property="og:description"
+					content="Get Discord avatar decorations without spending money"
+				/>
+
+				<meta
+					name="twitter:title"
+					content="Discord Fake Avatar Decorations"
+				/>
+				<meta
+					name="twitter:description"
+					content="Get Discord avatar decorations without spending money"
+				/>
+			</head>
 			<body className="bg-surface4">{children}</body>
 		</html>
 	);
