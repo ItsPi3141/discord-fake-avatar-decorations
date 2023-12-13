a9a6b6a56979b2eda21d3ba79335d3dfac7b9104
Pi <piisthebestthingintheuniverse@gmail.com>
2023-12-13 15:40:57 -0800

    add new monsters decoration

--- utils/getAvatarDecorations.js
+++ utils/getAvatarDecorations.js
@@ -1,6 +1,6 @@
 (() => {
 	var e = [];
-	document.querySelectorAll("div[class^='shopCard-'] svg:nth-child(2) div[class^='avatarStack-'] > img[class^='avatar-']").forEach((el) => {
+	document.querySelectorAll("div[class^='shopCard_'] svg:nth-child(2) div[class^='avatarStack_'] > img[class^='avatar_']").forEach((el) => {
 		e.push(el.getAttribute("src").replace("size=160&passthrough=false", "size=1024"));
 	});
 	console.log(e.join("\n"));
