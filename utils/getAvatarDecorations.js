a2cdee966a3be3ac814c9c75dde0dadb01ebdb27
Pi <piisthebestthingintheuniverse@gmail.com>
2023-12-13 17:00:13 -0800

    fixed new monster avatars, they are animated now

--- utils/getAvatarDecorations.js
+++ utils/getAvatarDecorations.js
@@ -1,7 +1,7 @@
 (() => {
 	var e = [];
 	document.querySelectorAll("div[class^='shopCard_'] svg:nth-child(2) div[class^='avatarStack_'] > img[class^='avatar_']").forEach((el) => {
-		e.push(el.getAttribute("src").replace("size=160&passthrough=false", "size=1024"));
+		e.push(el.getAttribute("src").replace(/size=\d{2,4}&passthrough=false/, "size=1024"));
 	});
 	console.log(e.join("\n"));
 })();
