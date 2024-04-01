9b9f9459af6e17bb72a631da00c9227c9d010ad5
Pi <piisthebestthingintheuniverse@gmail.com>
2024-04-01 16:51:41 -0700

    twemoji: switch to discord's twemoji package

--- app/components/twemoji.jsx
+++ app/components/twemoji.jsx
@@ -1,14 +1,14 @@
 // https://gist.github.com/chibicode/fe195d792270910226c928b69a468206
 import React, { memo } from "react";
-import twemoji from "twemoji";
+import twemoji from "@discordapp/twemoji";
 
 const Twemoji = ({ emoji }) => (
 	<span
 		dangerouslySetInnerHTML={{
 			__html: twemoji.parse(emoji, {
 				folder: "svg",
-				ext: ".svg"
-			})
+				ext: ".svg",
+			}),
 		}}
 	/>
 );
