059d545e03a6a9e00815cd23455d586793c19d90
Pi <piisthebestthingintheuniverse@gmail.com>
2024-04-26 19:01:23 -0700

    seo: improve description

--- app/layout.jsx
+++ app/layout.jsx
@@ -12,18 +12,18 @@ export default function RootLayout({ children }) {
 			<head>
 				{/* Meta tags */}
 				<title>Discord Fake Avatar Decorations</title>
-				<meta name="description" content="Get Discord avatar decorations without spending money" />
+				<meta name="description" content="Get Discord avatar decorations for free without spending money!" />
 
 				{/* Open Graph embeds */}
 				<meta property="og:type" content="website" />
 				<meta property="og:title" content="Discord Fake Avatar Decorations" />
-				<meta property="og:description" content="Get Discord avatar decorations without spending money" />
+				<meta property="og:description" content="Get Discord avatar decorations for free without spending money!" />
 				<meta property="og:image" content="https://discord-decorations.vercel.app/android-chrome-192x192.png" />
 
 				{/* Twitter embeds */}
 				<meta name="twitter:card" content="summary" />
 				<meta name="twitter:title" content="Discord Fake Avatar Decorations" />
-				<meta name="twitter:description" content="Get Discord avatar decorations without spending money" />
+				<meta name="twitter:description" content="Get Discord avatar decorations for free without spending money!" />
 				<meta property="twitter:image" content="https://discord-decorations.vercel.app/android-chrome-192x192.png" />
 
 				{/* Favicon */}
