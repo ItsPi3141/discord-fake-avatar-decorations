2c6a1b416482f67c576dc4b646b0d9cc25c243e1
Pi <piisthebestthingintheuniverse@gmail.com>
2024-06-27 20:30:04 -0700

    ffmpeg: fix transparency loss when adjusting avatar duration

--- ffmpeg/processImage.js
+++ ffmpeg/processImage.js
@@ -103,33 +103,59 @@ export function addDecoration(/** @type {FFmpeg} */ ffmpeg, /** @type {String} *
 				const decoAB = await (await fetch(decorationUrl)).arrayBuffer();
 				const decoData = await fetchFile(new Blob([decoAB]));
 
-				async function writeFiles() {
-					await ffmpeg.writeFile(`avatarbase.${ext}`, avatarData);
-				}
 				if (ext === "gif") {
 					const decoDuration = getAPngDuration(decoAB);
 					const avatarDuration = getGifDuration(avatarAB);
 					if (decoDuration > avatarDuration) {
 						await ffmpeg.writeFile("avatar_before_timing.gif", avatarData);
-						await ffmpeg.exec(["-ignore_loop", "0", "-i", "avatar_before_timing.gif", "-loop", "0", "-t", `${decoDuration}`, `avatarbase.${ext}`]);
+						console.log(decoDuration, avatarDuration);
+						await ffmpeg.exec([
+							"-stream_loop",
+							"-1",
+							"-i",
+							"avatar_before_timing.gif",
+							"-filter_complex",
+							[
+								`[0:v]trim=start=0:end=${decoDuration},`,
+								"setpts=PTS-STARTPTS,",
+								"split[s0][s1];",
+								"[s0]palettegen=reserve_transparent=on:transparency_color=ffffff[p];",
+								"[s1][p]paletteuse",
+							].join(" "),
+							`avatarbase.${ext}`,
+						]);
+						console.log("gif done");
 					} else {
-						await writeFiles();
+						await ffmpeg.writeFile(`avatarbase.${ext}`, avatarData);
 					}
 				} else if (ext === "png") {
 					const decoDuration = getAPngDuration(decoAB);
 					const avatarDuration = getAPngDuration(avatarAB);
 					if (decoDuration > avatarDuration && avatarDuration > 1) {
+						console.log(decoDuration, avatarDuration);
 						await ffmpeg.writeFile("avatar_before_timing.png", avatarData);
-						await ffmpeg.exec(["-i", "avatar_before_timing.png", "-loop", "0", "-t", `${decoDuration}`, `avatarbase.${ext}`]);
+						await ffmpeg.exec([
+							"-i",
+							"avatar_before_timing.png",
+							"-filter_complex",
+							[
+								`[0:v]trim=start=0:end=${decoDuration},`,
+								"setpts=PTS-STARTPTS,",
+								"split[s0][s1];",
+								"[s0]palettegen=reserve_transparent=on:transparency_color=ffffff[p];",
+								"[s1][p]paletteuse",
+							].join(" "),
+							`avatarbase.${ext}`,
+						]);
+						console.log("png done");
 					} else {
-						await writeFiles();
+						await ffmpeg.writeFile(`avatarbase.${ext}`, avatarData);
 					}
 				} else {
-					await writeFiles();
+					await ffmpeg.writeFile(`avatarbase.${ext}`, avatarData);
 				}
 				await ffmpeg.writeFile("decoration.png", decoData);
 
-				// run ffmpeg -i avatarbase.png -i decoration.png -filter_complex "[0][1]overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2:format=auto,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" avatarwithdeco.gif
 				const filter_complex = [
 					// Start out with a transparent background
 					"color=s=288x288:d=100,format=argb,colorchannelmixer=aa=0.0[background];",
@@ -163,12 +189,12 @@ export function addDecoration(/** @type {FFmpeg} */ ffmpeg, /** @type {String} *
 				await ffmpeg.exec(["-i", `avatarbase.${ext}`, "-i", "decoration.png", "-filter_complex", filter_complex.join(""), "avatarwithdeco.gif"]);
 
 				const res = await ffmpeg.readFile("avatarwithdeco.gif").catch((err) => console.error(err));
+				// const res = await ffmpeg.readFile(`avatarbase.${ext}`).catch((err) => console.error(err));
 				if (typeof res === "undefined" || res.length == 0) {
 					console.error("Error: Empty result from ffmpeg");
 					return reject();
 				}
 				const reader = new FileReader();
-				console.log(getGifDuration(res.buffer));
 				reader.readAsDataURL(new Blob([new Uint8Array(res.buffer, res.byteOffset, res.length)], { type: "image/gif" }));
 				reader.onload = () => {
 					return resolve(reader.result);
