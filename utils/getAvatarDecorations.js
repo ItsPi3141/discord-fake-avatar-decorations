ba1fcd4b61b8600fe397530cfd21344ce93d4d31
Pi <piisthebestthingintheuniverse@gmail.com>
2024-07-10 22:30:28 -0700

    decorations: add dark fantasy

--- utils/getAvatarDecorations.js
+++ utils/getAvatarDecorations.js
@@ -1,7 +1,11 @@
 (() => {
 	var e = [];
-	document.querySelectorAll("div[class^='shopCard_'] svg:nth-child(2) div[class^='avatarStack_'] > img[class^='avatar_']").forEach((el) => {
-		e.push(el.getAttribute("src").replace(/size=\d{2,4}&passthrough=false/, "size=1024"));
-	});
+	document
+		.querySelectorAll(
+			"div[class^='categories_'] div[class^='shopCard_'] div[class^='avatarContainer_'] svg:nth-child(2) div[class^='avatarStack_'] > img[class^='avatar_']"
+		)
+		.forEach((el) => {
+			e.push(el.getAttribute("src").replace(/size=\d{2,4}&passthrough=false/, "size=1024"));
+		});
 	console.log(e.join("\n"));
 })();
