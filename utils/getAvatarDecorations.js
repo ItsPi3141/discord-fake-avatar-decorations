9897c124e7251eb607fe9f0f8dc446f31461bac3
Pi <piisthebestthingintheuniverse@gmail.com>
2024-08-07 12:19:38 -0700

    decorations: add valorant champions collection

--- utils/getAvatarDecorations.js
+++ utils/getAvatarDecorations.js
@@ -1,11 +1,13 @@
 (() => {
-	var e = [];
-	document
-		.querySelectorAll(
-			"div[class^='categories_'] div[class^='shopCard_'] div[class^='avatarContainer_'] svg:nth-child(2) div[class^='avatarStack_'] > img[class^='avatar_']"
-		)
-		.forEach((el) => {
-			e.push(el.getAttribute("src").replace(/size=\d{2,4}&passthrough=false/, "size=1024"));
-		});
+	const e = [];
+	for (const el of document.querySelectorAll(
+		"div[class^='categories_'] div[class^='shopCard'] div[class^='avatarContainer_'] svg:nth-child(2) div[class^='avatarStack_'] > img[class^='avatar_']",
+	)) {
+		e.push(
+			el
+				.getAttribute("src")
+				.replace(/size=\d{2,4}&passthrough=false/, "size=1024"),
+		);
+	}
 	console.log(e.join("\n"));
 })();
