52bb94756b4295e052982d291bdb3550acd00a48
Pi <piisthebestthingintheuniverse@gmail.com>
2024-08-27 12:53:00 -0700

    title

--- app/layout.jsx
+++ app/layout.jsx
@@ -10,18 +10,18 @@ export default function RootLayout({ children }) {
 		<html lang="en">
 			<head>
 				{/* Meta tags */}
-				<title>Discord Fake Avatar Decorations</title>
+				<title>Fake Discord Avatar Decorations</title>
 				<meta name="description" content="Get Discord avatar decorations for free!" />
 
 				{/* Open Graph embeds */}
 				<meta property="og:type" content="website" />
-				<meta property="og:title" content="Discord Fake Avatar Decorations" />
+				<meta property="og:title" content="Fake Discord Avatar Decorations" />
 				<meta property="og:description" content="Get Discord avatar decorations for free!" />
 				<meta property="og:image" content="https://discord-decorations.vercel.app/android-chrome-192x192.png" />
 
 				{/* Twitter embeds */}
 				<meta name="twitter:card" content="summary" />
-				<meta name="twitter:title" content="Discord Fake Avatar Decorations" />
+				<meta name="twitter:title" content="Fake Discord Avatar Decorations" />
 				<meta name="twitter:description" content="Get Discord avatar decorations for free!" />
 				<meta property="twitter:image" content="https://discord-decorations.vercel.app/android-chrome-192x192.png" />
 
