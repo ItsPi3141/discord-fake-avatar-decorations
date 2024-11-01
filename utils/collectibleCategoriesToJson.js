77e1ebba9e1f85852b09eb4787fd1a53d50e9bd7
Pi <90981829+ItsPi3141@users.noreply.github.com>
2024-10-31 18:13:43 -0700

    add conversion script

---
+++ utils/collectibleCategoriesToJson.js
@@ -0,0 +1,21 @@
+function collectibleCategoriesToJson(collectibleCategories) {
+	return collectibleCategories.map((category) => ({
+		name: category.name,
+		description: category.summary,
+		banner: {
+			image: `/banners/${category.name.toLowerCase().replaceAll(" ", "_")}.png`,
+			text: `/bannertext/${category.name.toLowerCase().replaceAll(" ", "_")}.png`,
+			height: 50,
+		},
+		items: category.products
+			.filter((p) => p.type === 0)
+			.map((p) => ({
+				name: p.name,
+				description: p.summary,
+				file: `${p.name
+					.toLowerCase()
+					.replaceAll("'", "")
+					.replaceAll(/[^A-Za-z0-9]/g, "_")}.png`,
+			})),
+	}));
+}
