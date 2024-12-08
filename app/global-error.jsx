5b66a8022049ecc64b1582edc1f37b0805b4c903
Pi <90981829+ItsPi3141@users.noreply.github.com>
2024-12-08 15:40:28 -0800

    custom error page

---
+++ app/global-error.jsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function GlobalErrorPage() {
+	return (
+		<main className="top-0 right-0 bottom-0 left-0 fixed flex flex-col justify-center items-center text-white">
+			<h1 className="text-6xl ginto">500</h1>
+			<p className="text-center">Internal server error</p>
+		</main>
+	);
+}
---
+++ app/not-found.jsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function NotFoundPage() {
+	return (
+		<main className="top-0 right-0 bottom-0 left-0 fixed flex flex-col justify-center items-center text-white">
+			<h1 className="text-6xl ginto">404</h1>
+			<p className="text-center">Page not found</p>
+		</main>
+	);
+}
