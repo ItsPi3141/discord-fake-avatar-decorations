2abec7797f209cbf7107b729fc7579fd07df8e50
Pi <90981829+ItsPi3141@users.noreply.github.com>
2024-12-08 00:05:06 -0800

    fix b64 image loading error

--- app/components/image.jsx
+++ app/components/image.jsx
@@ -3,14 +3,14 @@ const baseImgUrl = process.env.NEXT_PUBLIC_BASE_IMAGE_URL || "";
 export default function Image(props) {
 	return (
 		<>
-			{props.src && (
+			{props.src !== "" && (
 				<img
 					{...props}
 					alt=""
 					src={
-						props.src.startsWith("http") ||
-						props.src.startsWith("data:") ||
-						`${baseImgUrl}${props.src}`
+						props.src.startsWith("http") || props.src.startsWith("data:")
+							? props.src
+							: `${baseImgUrl}${props.src}`
 					}
 				/>
 			)}
