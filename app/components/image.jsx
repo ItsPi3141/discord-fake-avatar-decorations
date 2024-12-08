f04e6aaa7bd33c57b8e83730364a2875f6110e68
Pi <90981829+ItsPi3141@users.noreply.github.com>
2024-12-08 00:01:43 -0800

    fix image error

--- app/components/image.jsx
+++ app/components/image.jsx
@@ -2,16 +2,18 @@ const baseImgUrl = process.env.NEXT_PUBLIC_BASE_IMAGE_URL || "";
 
 export default function Image(props) {
 	return (
-		props.src && (
-			<img
-				{...props}
-				alt=""
-				src={
-					props.src.startsWith("http") ||
-					props.src.startsWith("data:") ||
-					`${baseImgUrl}${props.src}`
-				}
-			/>
-		)
+		<>
+			{props.src && (
+				<img
+					{...props}
+					alt=""
+					src={
+						props.src.startsWith("http") ||
+						props.src.startsWith("data:") ||
+						`${baseImgUrl}${props.src}`
+					}
+				/>
+			)}
+		</>
 	);
 }
