818ee4eae28ea1d37a38f067f5fcfb04ada7b36f
ItsPi3141 <90981829+ItsPi3141@users.noreply.github.com>
2025-07-30 10:22:49 -0700

    fix: typo

--- build/generateWebmanifest.js
+++ build/generateWebmanifest.js
@@ -2,7 +2,7 @@ const base = process.env.VITE_BASE_IMAGE_URL || "";
 
 export const generateWebmanifest = () => {
 	return JSON.stringify({
-		name: "Discord Fake Avatar Decorations",
+		name: "Fake Discord Avatar Decorations",
 		short_name: "Discord Decorations",
 		icons: [
 			{
