823f92a2530da1698e7a63f929c3d405dd2b062a
ItsPi3141 <90981829+ItsPi3141@users.noreply.github.com>
2025-07-30 10:38:39 -0700

    perf: optimize font preloader

--- src/components/fontpreload.jsx
+++ src/components/fontpreload.jsx
@@ -16,21 +16,17 @@ export function FontPreloader() {
 						left: 0,
 						position: "fixed",
 						opacity: 0,
-						color: "transparent",
 						pointerEvents: "none",
+						fontSize: "1px",
 					}}
 				>
 					{[400, 500, 600, 700].map((weight) => (
-						<p key={weight} style={{ fontWeight: weight, fontSize: "1px" }}>
+						<p key={weight} style={{ fontWeight: weight }}>
 							e
 						</p>
 					))}
-					<p className="ginto" style={{ fontSize: "1px" }}>
-						e
-					</p>
-					<p className="nitro-font" style={{ fontSize: "1px" }}>
-						e
-					</p>
+					<p className="ginto">e</p>
+					<p className="nitro-font">e</p>
 				</div>
 			)}
 		</>
