49a979cb5ee51b313288555b62e7194ced832109
ItsPi3141 <90981829+ItsPi3141@users.noreply.github.com>
2025-08-16 15:48:02 -0700

    fix: make emojis undraggable

--- src/components/twemoji.jsx
+++ src/components/twemoji.jsx
@@ -3,6 +3,7 @@ import "./twemoji.css";
 const Twemoji = ({ emoji }) => (
   <img
     class={"emoji"}
+    draggable={false}
     src={`https://cdn.jsdelivr.net/gh/jdecked/twemoji@16.0.1/assets/svg/${emoji}.svg`}
   />
 );
