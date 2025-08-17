200926ae516f70e3347f5125517f019cbe2da114
ItsPi3141 <90981829+ItsPi3141@users.noreply.github.com>
2025-08-17 16:20:11 -0700

    feat: add lazy loading for twemoji

--- src/components/twemoji.jsx
+++ src/components/twemoji.jsx
@@ -5,6 +5,7 @@ const Twemoji = ({ emoji }) => (
     class={"emoji"}
     draggable={false}
     src={`https://cdn.jsdelivr.net/gh/jdecked/twemoji@16.0.1/assets/svg/${emoji}.svg`}
+    loading={"lazy"}
   />
 );
 
