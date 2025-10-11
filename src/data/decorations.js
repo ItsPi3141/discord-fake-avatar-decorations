c4602813e4309558db72b7c2ba17617929e3a068
ItsPi3141 <90981829+ItsPi3141@users.noreply.github.com>
2025-10-11 16:46:20 -0700

    fix: change trick or treat badge to fall 2025

--- src/data/decorations.js
+++ src/data/decorations.js
@@ -6,7 +6,7 @@ export const decorationsData = [
       {
         n: "Trick or Treat",
         b: "trick_or_treat",
-        badge: "OCTOBER 2025",
+        badge: "FALL 2025",
         i: [
           {
             n: "Trick Pumpkin",
